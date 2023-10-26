4267486110889592330a55d46b9bf1b6b5c33cc7
vtuan2456 <tuanron2004@gmail.com>
2023-10-27 01:23:28 +0700

    s

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,8 @@
-let list = document.querySelector('.slider .list');
-let items = document.querySelectorAll('.slider .list .item');
+let list = document.querySelector('.slider .lista');
+let items = document.querySelectorAll('.slider .lista .item');
 let dots = document.querySelectorAll('.slider .dots li');
 let prev = document.getElementById('prev');
 let next = document.getElementById('next');
-
 let active = 0;
 let lenghItems = items.length - 1;
 
