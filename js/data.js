63a77b2083400e3169136af3e06b846748e55908
aimeeceline <lelananh02@gmail.com>
2023-11-10 01:27:07 +0700

    haizz

---
+++ js/data.js
@@ -0,0 +1,135 @@
+[
+    {
+        "id": 1,
+        "type": "Kỹ năng sống - Phát triển cá nhân",
+        "name":"Chiến tranh tiền tệ",
+        "price": 200,
+        "image1": "/img/product/Kỹ năng sống - Phát triển cá nhân/Chiến tranh tiền tệ 1.jpg",
+        "image 2": "img/product/Kỹ năng sống - Phát triển cá nhân/ Chiến tranh tiền tệ 2.jpg",
+        "image 3": "img/product/Kỹ năng sống - Phát triển cá nhân/ Chiến tranh tiền tệ 3.jpg",
+        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 2,
+        "type": "Kỹ năng sống - Phát triển cá nhân",
+        "name":"Đắc Nhân Tâm",
+        "price": 200,
+        "image1": "/img/product/Kỹ năng sống - Phát triển cá nhân/Đắc Nhân Tâm 1.jpg",
+        "image 2": "img/product/Kỹ năng sống - Phát triển cá nhân/ Đắc Nhân Tâm 2.jpg",
+        "image 3": "img/product/Kỹ năng sống - Phát triển cá nhân/ Đắc Nhân Tâm 3.jpg",
+        "Nhà cung cấp": "CÔNG TY TNHH PHÁT HÀNH S BOOKS",
+        "Hình thức bìa": "Bìa Mềm",
+        "Tác giả": "Dale Carnegie",
+        "Nhà xuất bản": "Văn Học"
+    },
+    {
+        "id": 3,
+        "type": "Kỹ năng sống - Phát triển cá nhân",
+        "name":"Dám Bị Ghét",
+        "price": 200,
+        "image1": "/img/product/Kỹ năng sống - Phát triển cá nhân/Dám Bị Ghét 1.jpg",
+        "image 2": "img/product/Kỹ năng sống - Phát triển cá nhân/ Dám Bị Ghét 2.jpg",
+        "image 3": "img/product/Kỹ năng sống - Phát triển cá nhân/ Dám Bị Ghét 3.jpg",
+        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 4,
+        "type": "Kỹ năng sống - Phát triển cá nhân",
+        "name":"lãnh đạo tập sự",
+        "price": 200,
+        "image1": "/img/product/Kỹ năng sống - Phát triển cá nhân/lãnh đạo tập sự 1.jpg",
+        "image 2": "img/product/Kỹ năng sống - Phát triển cá nhân/ lãnh đạo tập sự 2.jpg",
+        "image 3": "img/product/Kỹ năng sống - Phát triển cá nhân/ lãnh đạo tập sự 3.jpg",
+        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 5,
+        "type": "Kỹ năng sống - Phát triển cá nhân",
+        "name":"Những người khốn khổ",
+        "price": 200,
+        "image1": "/img/product/Kỹ năng sống - Phát triển cá nhân/nhung nguoi 1.jpg",
+        "image 2": "img/product/Kỹ năng sống - Phát triển cá nhân/ nhung nguoi 2.jpg",
+        "image 3": "img/product/Kỹ năng sống - Phát triển cá nhân/ nhung nguoi 3.jpg",
+        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 6,
+        "type": "Kỹ năng sống - Phát triển cá nhân",
+        "name":"Nóng giận là bản năng, tĩnh lặng là bản lĩnh",
+        "price": 200,
+        "image1": "/img/product/Kỹ năng sống - Phát triển cá nhân/nóng giận 1.jpg",
+        "image 2": "img/product/Kỹ năng sống - Phát triển cá nhân/ nóng giận 2.jpg",
+        "image 3": "img/product/Kỹ năng sống - Phát triển cá nhân/ nóng giận 3.jpg",
+        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 7,
+        "type": "Kỹ năng sống - Phát triển cá nhân",
+        "name":"Ổn định hay tự do",
+        "price": 200,
+        "image1": "/img/product/Kỹ năng sống - Phát triển cá nhân/ổn định hay tự do 1.jpg",
+        "image 2": "img/product/Kỹ năng sống - Phát triển cá nhân/ ổn định hay tự do 2.jpg",
+        "image 3": "img/product/Kỹ năng sống - Phát triển cá nhân/ ổn định hay tự do 3.jpg",
+        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 8,
+        "type": "Kỹ năng sống - Phát triển cá nhân",
+        "name":"Sống thực tế giữa đời thực dụng",
+        "price": 200,
+        "image1": "/img/product/Kỹ năng sống - Phát triển cá nhân/Sống thực tế giữa đời thực dụng 1.jpg",
+        "image 2": "img/product/Kỹ năng sống - Phát triển cá nhân/ Sống thực tế giữa đời thực dụng 2.jpg",
+        "image 3": "img/product/Kỹ năng sống - Phát triển cá nhân/ Sống thực tế giữa đời thực dụng 3.jpg",
+        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 9,
+        "type": "Kỹ năng sống - Phát triển cá nhân",
+        "name":"Sức mạnh của sự tập trung 1",
+        "price": 200,
+        "image1": "/img/product/Kỹ năng sống - Phát triển cá nhân/sức mạnh của sự tập trung 1.jpg",
+        "image 2": "img/product/Kỹ năng sống - Phát triển cá nhân/ sức mạnh của sự tập trung 2.jpg",
+        "image 3": "img/product/Kỹ năng sống - Phát triển cá nhân/ sức mạnh của sự tập trung 3.jpg",
+        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 10,
+        "type": "Manga – Comic",
+        "name":"Chú thuật hồi chiến",
+        "price": 200,
+        "image1": "/img/product/Manga – Comic/Chú thuật hồi chiến 1.jpg",
+        "image 2": "img/product/Manga – Comic/ Chú thuật hồi chiến 2.jpg",
+        "image 3": "img/product/Manga – Comic/ Chú thuật hồi chiến 3.jpg",
+        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 11,
+        "type": "Manga – Comic",
+        "name":"Chuyện kỳ lạ ở tiệm sách cũ Tanabe",
+        "price": 200,
+        "image1": "img/product/Manga – Comic/ chuyện lạ 1.jpg",
+        "image 2": "img/product/Manga – Comic/ chuyện lạ 2.jpg",
+        "image 3": "img/product/Manga – Comic/ chuyện lạ 3.jpg",
+        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 12,
+        "type": "Manga – Comic",
+        "name":"Conan",
+        "price": 200,
+        "image1": "img/product/Manga – Comic/ conan 1.jpg",
+        "image 2": "img/product/Manga – Comic/ conan 2.jpg",
+        "image 3": "img/product/Manga – Comic/ conan 3.jpg",
+        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 13,
+        "type": "Manga – Comic",
+        "name":"Doraemon",
+        "price": 200,
+        "image1": "img/product/Manga – Comic/ doraemon 1.jpg",
+        "image 2": "img/product/Manga – Comic/ doraemon 2.jpg",
+        "image 3": "img/product/Manga – Comic/ doraemon 3.jpg",
+        "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    }
+]
