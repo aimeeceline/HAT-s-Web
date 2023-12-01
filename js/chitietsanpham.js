d2f5c30cbd6e892d34ef618e4d78c722d7710d7f
aimeeceline <lelananh02@gmail.com>
2023-12-01 20:59:51 +0700

    bổ sung chi tiet sp

--- js/chitietsanpham.js
+++ js/chitietsanpham.js
@@ -38,18 +38,33 @@
         detail.querySelector('.infor .description').innerText = thisProduct.description;
 
 
-        (products.filter(value => value.id != productId)).forEach(product => {
-                let newProduct = document.createElement('a');
-                newProduct.href = 'productdetail.html?id=' + product.id;
-                newProduct.classList.add('itema');
-                newProduct.innerHTML = `
-                    <img src="${product.image1}"></img>
-                    <div class="unmain">
-                        <p>${product.name}</p>
-                        <p><b>${product.price}</b></p>
-                    </div>`;
-                    listProduct.appendChild(newProduct);
+                // Shuffle function to randomize array
+        function shuffle(array) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            return array;
+        }
+
+        // Shuffle the products array and select the first 4 elements
+        const shuffledProducts = shuffle(products.filter(value => value.id !== productId));
+        const productsToShow = shuffledProducts.slice(0, 4);
+
+        productsToShow.forEach(product => {
+            let newProduct = document.createElement('a');
+            newProduct.href = 'productdetail.html?id=' + product.id;
+            newProduct.classList.add('itema');
+            newProduct.innerHTML = `
+                <img src="${product.image1}"></img>
+                <div class="unmain">
+                    <p>${product.name}</p>
+                    <p><b>${product.price}</b></p>
+                </div>`;
+            listProduct.appendChild(newProduct);
         });
+
+
     }
     
     
