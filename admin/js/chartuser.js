9bb8e0a9310d76ce262d44ef1aa3426704543315
aimeeceline <lelananh02@gmail.com>
2023-12-08 03:10:13 +0700

    haizz

--- admin/js/chartuser.js
+++ admin/js/chartuser.js
@@ -3,8 +3,8 @@ const myChart2 = new Chart(ctx2, {
   type: "bar",
   data: {
     labels: [
-      "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-      "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+      "T1", "T2", "T3", "T4", "T5", "T6",
+      "T7", "T8", "T9", "T10", "T11", "T12"
     ],
     datasets: [
       {
